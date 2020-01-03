c6e0d800728bf70efea3a3c5a4d71bcfee003922
longyou <longyou@qq.com>
2020-01-03 15:18:08 +0800

    更改resource地址

--- springcloud-tester/springcloud-config-tester/src/main/java/org/springcloud/config/BasicSecurityConfig.java
+++ springcloud-tester/springcloud-config-tester/src/main/java/org/springcloud/config/BasicSecurityConfig.java
@@ -1,15 +1,8 @@
 package org.springcloud.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
-
-import java.util.List;
 
 
 @Configuration
@@ -20,7 +13,10 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         http
-                .httpBasic().and().formLogin()
+                .httpBasic()
+                .and().formLogin()
+                .and().logout()
+                .and().authorizeRequests().anyRequest().authenticated()
                 .and().csrf().disable()
         ;
 
