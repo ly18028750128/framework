687b06af4a40e83326c2f7bc9b3133c9476cb346
longyou <longyou88888@126.com>
2020-02-15 13:48:13 +0800

    1：优化定时任务的代码

--- springcloud-tester/cloud-core/src/main/java/org/cloud/scheduler/service/QuartzService.java
+++ springcloud-tester/cloud-core/src/main/java/org/cloud/scheduler/service/QuartzService.java
@@ -4,6 +4,7 @@ import com.baomidou.dynamic.datasource.annotation.DS;
 import org.cloud.scheduler.controller.QuartzController;
 import org.quartz.*;
 import org.quartz.impl.matchers.GroupMatcher;
+import org.quartz.spi.MutableTrigger;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,10 +17,7 @@ import java.util.*;
 @Service
 @DS("quartz")
 public class QuartzService {
-
     final Logger logger = LoggerFactory.getLogger(QuartzService.class);
-
-
     @Autowired
     private Scheduler scheduler;
 
@@ -42,31 +40,37 @@ public class QuartzService {
      * @param jobTimes     运行的次数 （<0:表示不限次数）
      * @param jobData      参数
      */
-
     public void addJob(Class<? extends QuartzJobBean> jobClass, String jobName, String jobGroupName, int jobTime,
-                       int jobTimes, Map jobData) throws Exception {
+                       int jobTimes, Map<String, ?> jobData, Boolean isStartNow) throws Exception {
 
         // 任务名称和组构成任务key
-        JobDetail jobDetail = JobBuilder.newJob(jobClass).withIdentity(jobName, jobGroupName)
-                .build();
-        // 设置job参数
-        if (jobData != null && jobData.size() > 0) {
-            jobDetail.getJobDataMap().putAll(jobData);
-        }
+        JobDetail jobDetail = JobBuilder.newJob(jobClass).withIdentity(jobName, jobGroupName).build();
         // 使用simpleTrigger规则
         Trigger trigger = null;
-        if (jobTimes < 0) {
-            trigger = TriggerBuilder.newTrigger().withIdentity(jobName, jobGroupName)
-                    .withSchedule(SimpleScheduleBuilder.repeatSecondlyForever(1).withIntervalInSeconds(jobTime))
-                    .startNow().build();
-        } else {
-            trigger = TriggerBuilder
-                    .newTrigger().withIdentity(jobName, jobGroupName).withSchedule(
-                            SimpleScheduleBuilder.repeatSecondlyForever(1).withIntervalInSeconds(jobTime).withRepeatCount(jobTimes))
-                    .startNow().build();
-        }
-        scheduler.scheduleJob(jobDetail, trigger);
+        SimpleScheduleBuilder scheduleBuilder = SimpleScheduleBuilder.repeatSecondlyForever(1).withIntervalInSeconds(jobTime);
 
+        // 重复次数大于零
+        if (jobTimes > 0) {
+            scheduleBuilder.withRepeatCount(jobTimes);
+        }
+
+        TriggerBuilder triggerBuilder = TriggerBuilder.newTrigger().withIdentity(jobName, jobGroupName)
+                .withDescription(getDescription(jobData)).withSchedule(scheduleBuilder);
+
+        // 是否增加时就启动
+        if (isStartNow) {
+            triggerBuilder = triggerBuilder.startNow();
+        }
+        trigger = triggerBuilder.build();
+
+        // 设置job参数
+        if (jobData != null && jobData.size() > 0) {
+            trigger.getJobDataMap().putAll(jobData);
+            jobDetail.getJobDataMap().putAll(jobData);
+        }
+
+        scheduler.scheduleJob(jobDetail, trigger);
+//        this.pauseJob(jobName, jobGroupName);  // 注册完后先暂停
     }
 
     /**
@@ -78,27 +82,37 @@ public class QuartzService {
      * @param jobTime      时间表达式 （如：0/5 * * * * ? ）
      * @param jobData      参数
      */
-
-    public void addJob(Class<? extends QuartzJobBean> jobClass, String jobName, String jobGroupName, String jobTime, Map jobData) throws Exception {
+    public void addJob(Class<? extends QuartzJobBean> jobClass, String jobName,
+                       String jobGroupName, String jobTime, Map<String, ?> jobData,
+                       Boolean isStartNow) throws Exception {
 
         // 创建jobDetail实例，绑定Job实现类
         // 指明job的名称，所在组的名称，以及绑定job类
         // 任务名称和组构成任务key
-        JobDetail jobDetail = JobBuilder.newJob(jobClass).withIdentity(jobName, jobGroupName)
-                .build();
-        // 设置job参数
-        if (jobData != null && jobData.size() > 0) {
-            jobDetail.getJobDataMap().putAll(jobData);
-        }
+        JobDetail jobDetail = JobBuilder.newJob(jobClass).withIdentity(jobName, jobGroupName).build();
         // 定义调度触发规则
         // 使用cornTrigger规则
         // 触发器key
-        Trigger trigger = TriggerBuilder.newTrigger().withIdentity(jobName, jobGroupName)
-                .startAt(DateBuilder.futureDate(1, DateBuilder.IntervalUnit.SECOND))
-                .withSchedule(CronScheduleBuilder.cronSchedule(jobTime)).startNow().build();
+        TriggerBuilder triggerBuilder = TriggerBuilder.newTrigger().withIdentity(jobName, jobGroupName)
+                .withDescription(getDescription(jobData)).startAt(DateBuilder.futureDate(1, DateBuilder.IntervalUnit.SECOND))
+                .withSchedule(CronScheduleBuilder.cronSchedule(jobTime));
+
+        // 是否增加时就启动
+        if (isStartNow) {
+            triggerBuilder = triggerBuilder.startNow();
+        }
+
+        Trigger trigger = triggerBuilder.build();
+
+        // 设置job参数
+        if (jobData != null && jobData.size() > 0) {
+            trigger.getJobDataMap().putAll(jobData);
+            jobDetail.getJobDataMap().putAll(jobData);
+        }
+
         // 把作业和触发器注册到任务调度中
         scheduler.scheduleJob(jobDetail, trigger);
-
+//        this.pauseJob(jobName, jobGroupName); // 注册完后先暂停，调用启动才可以正式的启用
     }
 
     /**
@@ -108,8 +122,8 @@ public class QuartzService {
      * @param jobGroupName
      * @param jobTime
      */
-
-    public void updateJob(String jobName, String jobGroupName, String jobTime) throws Exception {
+    public void updateJob(String jobName, String jobGroupName, String jobTime, Map<String, ?> jobData,
+                          Boolean isStartNow) throws Exception {
 
         TriggerKey triggerKey = TriggerKey.triggerKey(jobName, jobGroupName);
         Trigger trigger = scheduler.getTrigger(triggerKey);
@@ -117,9 +131,20 @@ public class QuartzService {
             throw new Exception("非按corn表达式执行的定时任务，不能更改时间表达式，如果要更改定时任务的类型请先删除后再新加！");
         }
         CronTrigger cronTrigger = (CronTrigger) trigger;
-        cronTrigger = cronTrigger.getTriggerBuilder().withIdentity(triggerKey).withSchedule(CronScheduleBuilder.cronSchedule(jobTime)).build();
+        TriggerBuilder<CronTrigger> triggerBuilder = cronTrigger.getTriggerBuilder().withIdentity(triggerKey).
+                withDescription(getDescription(jobData)).withSchedule(CronScheduleBuilder.cronSchedule(jobTime));
+
+        if (isStartNow) {
+            triggerBuilder.startNow();
+        }
+        cronTrigger = triggerBuilder.build();
+        cronTrigger.getJobDataMap().clear();
+        if (jobData != null) {
+            cronTrigger.getJobDataMap().putAll(jobData);
+        }
         // 重启触发器
         scheduler.rescheduleJob(triggerKey, cronTrigger);
+
     }
 
     /**
@@ -131,26 +156,30 @@ public class QuartzService {
      * @param jobTimes
      * @throws Exception
      */
-    public void updateJob(String jobName, String jobGroupName, int jobTime, int jobTimes) throws Exception {
-
+    public void updateJob(String jobName, String jobGroupName, int jobTime, int jobTimes, Map<String, ?> jobData,
+                          Boolean isStartNow) throws Exception {
         TriggerKey triggerKey = TriggerKey.triggerKey(jobName, jobGroupName);
-
         Trigger trigger = scheduler.getTrigger(triggerKey);
         if (!(trigger instanceof SimpleTrigger)) {
             throw new Exception("非按时间间隔执行的定时任务，不能更改间隔时间，如果要更改定时任务的类型请先删除后再新加！");
         }
         SimpleTrigger simpleTrigger = (SimpleTrigger) trigger;
 
-        if (jobTimes < 0) {
-            simpleTrigger = simpleTrigger.getTriggerBuilder().withIdentity(triggerKey).
-                    withSchedule(SimpleScheduleBuilder.repeatSecondlyForever(1).withIntervalInSeconds(jobTime))
-                    .build();
-        } else {
-            simpleTrigger = simpleTrigger.getTriggerBuilder().withIdentity(triggerKey)
-                    .withSchedule(SimpleScheduleBuilder.repeatSecondlyForever(1).withIntervalInSeconds(jobTime).withRepeatCount(jobTimes))
-                    .build();
+        SimpleScheduleBuilder scheduleBuilder = SimpleScheduleBuilder.repeatSecondlyForever(1).withIntervalInSeconds(jobTime);
+        if (jobTimes > 0) {
+            scheduleBuilder.withRepeatCount(jobTimes);
         }
+        TriggerBuilder<SimpleTrigger> triggerBuilder = simpleTrigger.getTriggerBuilder().withIdentity(triggerKey).withDescription(getDescription(jobData)).withSchedule(scheduleBuilder);
 
+        if (isStartNow) {
+            triggerBuilder.startNow();
+        }
+        simpleTrigger = triggerBuilder.build();
+
+        simpleTrigger.getJobDataMap().clear();
+        if (jobData != null) {
+            simpleTrigger.getJobDataMap().putAll(jobData);
+        }
         // 重启触发器
         scheduler.rescheduleJob(triggerKey, simpleTrigger);
     }
@@ -163,7 +192,6 @@ public class QuartzService {
      */
 
     public void deleteJob(String jobName, String jobGroupName) throws Exception {
-
         scheduler.deleteJob(new JobKey(jobName, jobGroupName));
 
     }
@@ -174,10 +202,20 @@ public class QuartzService {
      * @param jobName
      * @param jobGroupName
      */
-
     public void pauseJob(String jobName, String jobGroupName) throws Exception {
         JobKey jobKey = JobKey.jobKey(jobName, jobGroupName);
         scheduler.pauseJob(jobKey);
+    }
+
+    /**
+     * 暂停一个任务
+     *
+     * @param jobName
+     * @param jobGroupName
+     */
+    public void pauseTrigger(String jobName, String jobGroupName) throws Exception {
+        TriggerKey triggerKey = TriggerKey.triggerKey(jobName, jobGroupName);
+        scheduler.pauseTrigger(triggerKey);
     }
 
     /**
@@ -190,6 +228,18 @@ public class QuartzService {
     public void resumeJob(String jobName, String jobGroupName) throws Exception {
         JobKey jobKey = JobKey.jobKey(jobName, jobGroupName);
         scheduler.resumeJob(jobKey);
+    }
+
+    /**
+     * 恢复一个Trigger
+     *
+     * @param jobName
+     * @param jobGroupName
+     */
+
+    public void resumeTrigger(String jobName, String jobGroupName) throws Exception {
+        TriggerKey triggerKey = TriggerKey.triggerKey(jobName, jobGroupName);
+        scheduler.resumeTrigger(triggerKey);
     }
 
     /**
@@ -212,35 +262,14 @@ public class QuartzService {
 
     public List<Map<String, Object>> queryAllJob() throws Exception {
         List<Map<String, Object>> jobList = null;
-
         GroupMatcher<JobKey> matcher = GroupMatcher.anyJobGroup();
         Set<JobKey> jobKeys = scheduler.getJobKeys(matcher);
         jobList = new ArrayList<Map<String, Object>>();
         for (JobKey jobKey : jobKeys) {
             List<? extends Trigger> triggers = scheduler.getTriggersOfJob(jobKey);
             for (Trigger trigger : triggers) {
-                Map<String, Object> map = new HashMap<>();
-//                map.put(QuartzController.JobFieldName.CLASSNAME.value(), );
-                map.put(QuartzController.JobFieldName.JOBNAME.value(), jobKey.getName());
-                map.put(QuartzController.JobFieldName.JOBGROUPNAME.value(), jobKey.getGroup());
-                map.put(QuartzController.JobFieldName.DESCRIPTION.value(), "触发器[" + trigger.getKey() + "]");
-                map.put(QuartzController.JobFieldName.JOBDATA.value(), trigger.getJobDataMap());
-                Trigger.TriggerState triggerState = scheduler.getTriggerState(trigger.getKey());
-                map.put(QuartzController.JobFieldName.JOBSTATUS.value(), triggerState.name());
-                if (trigger instanceof CronTrigger) {
-                    CronTrigger cronTrigger = (CronTrigger) trigger;
-                    String cronExpression = cronTrigger.getCronExpression();
-                    map.put(QuartzController.JobFieldName.JOBTIME.value(), cronExpression);
-                } else if (trigger instanceof SimpleTrigger) {
-                    SimpleTrigger simpleTrigger = (SimpleTrigger) trigger;
-                    map.put("repeatCount", simpleTrigger.getRepeatCount());
-                    map.put("repeatInterval", simpleTrigger.getRepeatInterval());
-                }
-                map.put("nextFireTime", trigger.getNextFireTime());
-                map.put("previousFireTime", trigger.getPreviousFireTime());
-//                map.put("endTime", trigger.getEndTime());
-
-
+                Map<String, Object> map = new LinkedHashMap<>();
+                putPublicAttr(trigger, jobKey, map);
                 jobList.add(map);
             }
         }
@@ -257,31 +286,44 @@ public class QuartzService {
         List<JobExecutionContext> executingJobs = scheduler.getCurrentlyExecutingJobs();
         List<Map<String, Object>> jobList = new ArrayList<Map<String, Object>>(executingJobs.size());
         for (JobExecutionContext executingJob : executingJobs) {
-            Map<String, Object> map = new HashMap<String, Object>();
+            Map<String, Object> map = new LinkedHashMap<String, Object>();
             JobDetail jobDetail = executingJob.getJobDetail();
             JobKey jobKey = jobDetail.getKey();
             Trigger trigger = executingJob.getTrigger();
+            putPublicAttr(trigger, jobKey, map);
             map.put(QuartzController.JobFieldName.CLASSNAME.value(), jobDetail.getJobClass());
-            map.put(QuartzController.JobFieldName.JOBNAME.value(), jobKey.getName());
-            map.put(QuartzController.JobFieldName.JOBGROUPNAME.value(), jobKey.getGroup());
-            map.put(QuartzController.JobFieldName.DESCRIPTION.value(), "触发器[" + trigger.getKey() + "]");
             map.put(QuartzController.JobFieldName.JOBDATA.value(), jobDetail.getJobDataMap());
-            Trigger.TriggerState triggerState = scheduler.getTriggerState(trigger.getKey());
-            map.put(QuartzController.JobFieldName.JOBSTATUS.value(), triggerState.name());
-            if (trigger instanceof CronTrigger) {
-                CronTrigger cronTrigger = (CronTrigger) trigger;
-                String cronExpression = cronTrigger.getCronExpression();
-                map.put(QuartzController.JobFieldName.JOBTIME.value(), cronExpression);
-            } else if (trigger instanceof SimpleTrigger) {
-                SimpleTrigger simpleTrigger = (SimpleTrigger) trigger;
-                map.put("repeatCount", simpleTrigger.getRepeatCount());
-                map.put("repeatInterval", simpleTrigger.getRepeatInterval());
-            }
-            map.put("nextFireTime", trigger.getNextFireTime());
-            map.put("previousFireTime", trigger.getPreviousFireTime());
-//            map.put("endTime", trigger.getEndTime());
             jobList.add(map);
         }
         return jobList;
     }
+
+    private void putPublicAttr(Trigger trigger, JobKey jobKey, Map<String, Object> map) throws Exception {
+        map.put(QuartzController.JobFieldName.JOBNAME.value(), jobKey.getName());
+        map.put(QuartzController.JobFieldName.JOBGROUPNAME.value(), jobKey.getGroup());
+        map.put(QuartzController.JobFieldName.DESCRIPTION.value(), "触发器[" + trigger.getKey() + "]");
+        Trigger.TriggerState triggerState = scheduler.getTriggerState(trigger.getKey());
+        map.put(QuartzController.JobFieldName.JOBSTATUS.value(), triggerState.name());
+        if (trigger instanceof CronTrigger) {
+            CronTrigger cronTrigger = (CronTrigger) trigger;
+            String cronExpression = cronTrigger.getCronExpression();
+            map.put(QuartzController.JobFieldName.JOBTIME.value(), cronExpression);
+        } else if (trigger instanceof SimpleTrigger) {
+            SimpleTrigger simpleTrigger = (SimpleTrigger) trigger;
+            map.put("timesTriggered", simpleTrigger.getTimesTriggered());
+            map.put("repeatCount", simpleTrigger.getRepeatCount());
+            map.put("repeatInterval", simpleTrigger.getRepeatInterval());
+        }
+        map.put(QuartzController.JobFieldName.JOBDATA.value(), trigger.getJobDataMap());
+        map.put("nextFireTime", trigger.getNextFireTime());
+        map.put("previousFireTime", trigger.getPreviousFireTime());
+        map.put("finalFireTime", trigger.getFinalFireTime());
+        map.put("description", trigger.getDescription());
+    }
+
+    private String getDescription(Map datas) {
+        Object description = datas.get(QuartzController.JobFieldName.DESCRIPTION.value());
+        return description == null ? "" : description.toString();
+    }
+
 }
