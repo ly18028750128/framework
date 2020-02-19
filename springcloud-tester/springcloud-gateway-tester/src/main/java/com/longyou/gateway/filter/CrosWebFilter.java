77806998c2449f0d056483d473a00a8900d90ffd
longyou <longyou88888@126.com>
2020-02-20 00:08:49 +0800

    1：增加网关当前登录用户获取的方法

--- springcloud-tester/springcloud-gateway-tester/src/main/java/com/longyou/gateway/filter/CrosWebFilter.java
+++ springcloud-tester/springcloud-gateway-tester/src/main/java/com/longyou/gateway/filter/CrosWebFilter.java
@@ -1,10 +1,16 @@
 package com.longyou.gateway.filter;
 
 import com.longyou.gateway.config.vo.CorsConfigVO;
+import org.cloud.context.RequestContext;
+import org.cloud.context.RequestContextManager;
+import org.cloud.entity.LoginUserDetails;
+import org.cloud.utils.CommonUtil;
+import org.cloud.utils.RestTemplateUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.cors.reactive.CorsUtils;
@@ -18,12 +24,11 @@ public class CrosWebFilter implements WebFilter {
     @Autowired
     private CorsConfigVO corsConfigVO;
 
-    public static ThreadLocal<ServerWebExchange> serverWebExchangeThreadLocal=new ThreadLocal<>();
+    public static ThreadLocal<ServerWebExchange> serverWebExchangeThreadLocal = new ThreadLocal<>();
+
     @Override
     public Mono<Void> filter(ServerWebExchange swe, WebFilterChain wfc) {
-
         serverWebExchangeThreadLocal.set(swe);
-
         ServerHttpRequest request = swe.getRequest();
         if (CorsUtils.isCorsRequest(request)) {
             ServerHttpResponse response = swe.getResponse();
@@ -45,6 +50,29 @@ public class CrosWebFilter implements WebFilter {
         if (uri.startsWith("//")) {
             ServerHttpRequest newRequest = swe.getRequest().mutate().path(uri.replaceFirst("//", "/")).build();
             return wfc.filter(swe.mutate().request(newRequest).build());
+        }
+
+        if (request.getURI().toString().endsWith("/user/info/authentication")) {
+            return wfc.filter(swe);
+        }
+
+        RequestContext requestContext = new RequestContext();
+        final String userinfoUrl = CommonUtil.single().getEnv("system.userinfo.get_url", "http://SPRING-GATEWAY/user/info/authentication");
+        HttpHeaders headers = new HttpHeaders();
+        if (swe.getRequest().getHeaders().getFirst("cookie") != null) {
+            headers.add("cookie", swe.getRequest().getHeaders().getFirst("cookie"));
+        }
+        if (swe.getRequest().getHeaders().getFirst("authorization") != null) {
+            headers.add("authorization", swe.getRequest().getHeaders().getFirst("authorization"));
+        }
+        RequestContextManager.single().setRequestContext(requestContext);
+        if (headers.size() > 0) {
+            ResponseEntity<LoginUserDetails> responseEntity = RestTemplateUtil.single().getResponse(userinfoUrl, HttpMethod.GET, swe.getRequest().getHeaders(), LoginUserDetails.class);
+            LoginUserDetails user = responseEntity.getBody();
+            if (user != null) {
+                requestContext.setUser(user);
+            }
+            RequestContextManager.single().setRequestContext(requestContext);
         }
         return wfc.filter(swe);
     }
