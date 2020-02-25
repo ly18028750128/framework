14d93f247232db6d711e29949f163ded9cb3a256
longyou <longyou88888@126.com>
2020-02-25 17:25:35 +0800

    1:将网关改成无事物的模式

--- springcloud-tester/cloud-core/src/main/java/org/cloud/org/cloud/redis/RedisSessionConfig.java
+++ springcloud-tester/cloud-core/src/main/java/org/cloud/org/cloud/redis/RedisSessionConfig.java
@@ -3,8 +3,8 @@ package org.cloud.org.cloud.redis;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
-@Configuration
-@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 1800, redisNamespace = "system:spring:session")
+//@Configuration
+//@EnableRedisHttpSession
 public class RedisSessionConfig {
 
 }
