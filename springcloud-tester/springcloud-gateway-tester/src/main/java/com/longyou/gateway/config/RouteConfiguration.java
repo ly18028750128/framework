f2bd05680ef988b951fc43bd0feb88b093ed7e0d
longyou <longyou88888@126.com>
2020-02-26 10:08:06 +0800

    1:升级springcloud

--- springcloud-tester/springcloud-gateway-tester/src/main/java/com/longyou/gateway/config/RouteConfiguration.java
+++ springcloud-tester/springcloud-gateway-tester/src/main/java/com/longyou/gateway/config/RouteConfiguration.java
@@ -4,6 +4,7 @@ import com.longyou.gateway.config.vo.CorsConfigVO;
 import com.longyou.gateway.filter.CrosWebFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
 import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
 import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
 import org.springframework.context.annotation.Bean;
@@ -55,7 +56,7 @@ public class RouteConfiguration {
      * 如果使用了注册中心（如：Eureka），进行控制则需要增加如下配置
      */
     @Bean
-    public RouteDefinitionLocator discoveryClientRouteDefinitionLocator(DiscoveryClient discoveryClient) {
+    public RouteDefinitionLocator discoveryClientRouteDefinitionLocator(ReactiveDiscoveryClient discoveryClient) {
         return new DiscoveryClientRouteDefinitionLocator(discoveryClient, null);
     }
 }
