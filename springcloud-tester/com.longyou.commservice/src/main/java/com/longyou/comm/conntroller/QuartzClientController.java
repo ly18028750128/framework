6b0d399621159f237ceb26e5eb3ce0aaf7f14335
longyou <longyou88888@126.com>
2020-03-02 14:30:06 +0800

    1:开发环境时仅显示和自己相关的group的定时任务

--- springcloud-tester/com.longyou.commservice/src/main/java/com/longyou/comm/conntroller/QuartzClientController.java
+++ springcloud-tester/com.longyou.commservice/src/main/java/com/longyou/comm/conntroller/QuartzClientController.java
@@ -32,11 +32,18 @@ public class QuartzClientController {
 
     @GetMapping(value = "/getAll", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseResult getQuartzJobs() throws Exception {
+        String applicationGroup = CommonUtil.single().getEnv("spring.application.group", "");
         ResponseResult successResult = ResponseResult.createSuccessResult();
         Map<String, List<Map>> quartzJobs = new HashMap<>();
         List<Callable<Boolean>> callables = new ArrayList<>();
         List<String> services = discoveryClient.getServices();
         for (String serviceId : services) {
+
+            if(!"".equals(applicationGroup) && !serviceId.toUpperCase().startsWith(applicationGroup)){
+                continue;
+            }
+
+
             callables.add(new ProcessCallable<Boolean>() {
                 @Override
                 public Boolean process() {
@@ -55,8 +62,6 @@ public class QuartzClientController {
             });
         }
         ProcessUtil.single().runCablles(callables, 10, 180L);
-
-        String applicationGroup = CommonUtil.single().getEnv("spring.application.group", "");
 
         Map<String, List<Map>> sortResults = quartzJobs.entrySet()
                 .stream()
