87b458dc38ecc17b709e34a3f2dcfd645297adb2
longyou <longyou@qq.com>
2020-03-17 17:15:40 +0800

    1： 自定义查询数据备份

--- doc/db/mongo/datainterface备份.js
+++ doc/db/mongo/datainterface备份.js
@@ -1,90 +1,104 @@
-db.getCollection("dataInterFaceVO").insert( {
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : 172.16.4.22
+ Source Server Type    : MongoDB
+ Source Server Version : 40013
+ Source Host           : 172.16.4.22:27017
+ Source Schema         : test
+
+ Target Server Type    : MongoDB
+ Target Server Version : 40000
+ File Encoding         : 65001
+
+ Date: 17/03/2020 17:08:44
+*/
+
+
+// ----------------------------
+// Collection structure for dataInterFaceVO
+// ----------------------------
+db.getCollection("dataInterFaceVO").drop();
+db.createCollection("dataInterFaceVO");
+db.getCollection("dataInterFaceVO").createIndex({
+    md5: NumberInt("1")
+}, {
+    name: "md5_1",
+    unique: true
+});
+db.getCollection("dataInterFaceVO").createIndex({
+    interfaceName: NumberInt("1")
+}, {
+    name: "interfaceName_1",
+    unique: true
+});
+
+// ----------------------------
+// Documents of dataInterFaceVO
+// ----------------------------
+
+db.getCollection("dataInterFaceVO").insert([ {
+    _id: ObjectId("5e69ec77543ee56a391f1fcb"),
     interfaceName: "动态SQL示例所有登录用户",
     interfaceType: "sql",
     params: [ ],
     urlOrSqlContent: "select * from t_frame_user \n<where> \n\t<if test=\"userName!=null\">\n\t\tuser_name = #{userName,jdbcType=VARCHAR}\n\t</if>\n\t<if test=\"password!=null\">\n\t   and\tpassword = #{password,jdbcType=VARCHAR}\n\t</if>\n\t<if test=\"sexList!=null\">\n\t and sex in \n\t\t  <foreach item=\"item\" collection=\"sexList\" separator=\",\" open=\"(\" close=\")\" index=\"\">\n\t\t   #{item, jdbcType=VARCHAR}\n\t\t </foreach>\n\t</if>\n</where>",
     microServiceName: "COMMONSERVICE",
     authMethod: "ALLSYSTEMUSER",
-    createdOrUpdateTime: ISODate("2020-03-13T04:44:51.336Z"),
+    createdOrUpdateTime: ISODate("2020-03-12T09:07:04.468Z"),
     createdOrUpdateBy: NumberLong("1"),
     createdOrUpdateUserName: "admin",
     status: NumberInt("1"),
     _class: "org.cloud.mongo.DataInterFaceVO",
     restMethod: null,
     md5: "50d55445206d7a70022d932c98e2bd7d"
-} );
-
-db.getCollection("dataInterFaceVO").insert( {
+}, {
+    _id: ObjectId("5e69ec93543ee56a391f1fe2"),
     interfaceName: "动态SQL仅查询当前用户",
     interfaceType: "sql",
-    params: [
-        {
-            seq: NumberInt("0"),
-            fieldName: "121",
-            fieldType: "String",
-            description: "1231312",
-            status: NumberInt("1")
-        },
-        {
-            seq: NumberInt("5"),
-            fieldName: "12331",
-            fieldType: "String",
-            description: "12312",
-            status: NumberInt("1")
-        },
-        {
-            seq: NumberInt("15"),
-            fieldName: "12321",
-            fieldType: "String",
-            description: "123",
-            status: NumberInt("1")
-        }
-    ],
+    params: [ ],
     urlOrSqlContent: "select * from t_frame_user \n<where> \n\t<if test=\"userName!=null\">\n\t\tuser_name = #{userName,jdbcType=VARCHAR}\n\t</if>\n\tand id= #{paramsCurrentUserId,jdbcType=BIGINT}\n\t<if test=\"password!=null\">\n\t   and\tpassword = #{password,jdbcType=VARCHAR}\n\t</if>\n\t<if test=\"sexList!=null\">\n\t and sex in \n\t\t  <foreach item=\"item\" collection=\"sexList\" separator=\",\" open=\"(\" close=\")\" index=\"\">\n\t\t   #{item, jdbcType=VARCHAR}\n\t\t </foreach>\n\t</if>\n</where>",
     microServiceName: "COMMONSERVICE",
     authMethod: "ALLSYSTEMUSER",
-    createdOrUpdateTime: ISODate("2020-03-13T07:31:39.017Z"),
+    createdOrUpdateTime: ISODate("2020-03-12T09:05:42.503Z"),
     createdOrUpdateBy: NumberLong("1"),
     createdOrUpdateUserName: "admin",
     status: NumberInt("1"),
     _class: "org.cloud.mongo.DataInterFaceVO",
     restMethod: null,
     md5: "23450f7c37ba735d498a0be8575baf2d"
-} );
-
-db.getCollection("dataInterFaceVO").insert( {
+}, {
+    _id: ObjectId("5e69ecb0543ee56a391f1ff9"),
     interfaceName: "动态SQL无需授权示例",
     interfaceType: "sql",
     params: [ ],
     urlOrSqlContent: "select * from t_frame_user \n<where> \n\t<if test=\"userName!=null\">\n\t\tuser_name = #{userName,jdbcType=VARCHAR}\n\t</if>\n\t<if test=\"password!=null\">\n\t   and\tpassword = #{password,jdbcType=VARCHAR}\n\t</if>\n\t<if test=\"sexList!=null\">\n\t and sex in \n\t\t  <foreach item=\"item\" collection=\"sexList\" separator=\",\" open=\"(\" close=\")\" index=\"\">\n\t\t   #{item, jdbcType=VARCHAR}\n\t\t </foreach>\n\t</if>\n</where>",
     microServiceName: "COMMONSERVICE",
     authMethod: "NOAUTH",
-    createdOrUpdateTime: ISODate("2020-03-14T02:25:50.794Z"),
+    createdOrUpdateTime: ISODate("2020-03-12T09:09:58.275Z"),
     createdOrUpdateBy: NumberLong("1"),
     createdOrUpdateUserName: "admin",
     status: NumberInt("1"),
     _class: "org.cloud.mongo.DataInterFaceVO",
     restMethod: null,
     md5: "da1664b5771bce00f4919c0b1942cdcf"
-} );
-
-db.getCollection("dataInterFaceVO").insert( {
+}, {
+    _id: ObjectId("5e69ecc8543ee56a391f2010"),
     interfaceName: "动态SQL需要授权给用户的示例",
     interfaceType: "sql",
     params: [ ],
     urlOrSqlContent: "select * from t_frame_user \n<where> \n\t<if test=\"userName!=null\">\n\t\tuser_name = #{userName,jdbcType=VARCHAR}\n\t</if>\n\t<if test=\"password!=null\">\n\t   and\tpassword = #{password,jdbcType=VARCHAR}\n\t</if>\n\t<if test=\"sexList!=null\">\n\t and sex in \n\t\t  <foreach item=\"item\" collection=\"sexList\" separator=\",\" open=\"(\" close=\")\" index=\"\">\n\t\t   #{item, jdbcType=VARCHAR}\n\t\t </foreach>\n\t</if>\n</where>",
     microServiceName: "COMMONSERVICE",
     authMethod: "BYUSERPERMISSION",
-    createdOrUpdateTime: ISODate("2020-03-13T04:41:30.731Z"),
+    createdOrUpdateTime: ISODate("2020-03-12T09:07:08.18Z"),
     createdOrUpdateBy: NumberLong("1"),
     createdOrUpdateUserName: "admin",
     status: NumberInt("1"),
     _class: "org.cloud.mongo.DataInterFaceVO",
     restMethod: null,
     md5: "6191e5feb221dd9d0acb24ae2895fb80"
-} );
-
-db.getCollection("dataInterFaceVO").insert( {
+}, {
+    _id: ObjectId("5e6f56df0c730000a10008d9"),
     interfaceName: "查询系统角色列表",
     interfaceType: "sql",
     params: [ ],
@@ -98,9 +112,8 @@ db.getCollection("dataInterFaceVO").insert( {
     status: NumberInt("1"),
     _class: "org.cloud.mongo.DataInterFaceVO",
     restMethod: null
-} );
-
-db.getCollection("dataInterFaceVO").insert( {
+}, {
+    _id: ObjectId("5e6f56df0c730000a10008da"),
     interfaceName: "查询需要授权的资源清单列表",
     interfaceType: "sql",
     params: [ ],
@@ -114,9 +127,8 @@ db.getCollection("dataInterFaceVO").insert( {
     status: NumberInt("1"),
     _class: "org.cloud.mongo.DataInterFaceVO",
     restMethod: null
-} );
-
-db.getCollection("dataInterFaceVO").insert( {
+}, {
+    _id: ObjectId("5e6f56df0c730000a10008db"),
     interfaceName: "查询当前角色操作权限列表",
     interfaceType: "sql",
     params: [ ],
@@ -130,9 +142,8 @@ db.getCollection("dataInterFaceVO").insert( {
     status: NumberInt("1"),
     _class: "org.cloud.mongo.DataInterFaceVO",
     restMethod: null
-} );
-
-db.getCollection("dataInterFaceVO").insert( {
+}, {
+    _id: ObjectId("5e6f56df0c730000a10008dc"),
     interfaceName: "查询需要授权的接口清单",
     interfaceType: "sql",
     params: [ ],
@@ -145,84 +156,8 @@ db.getCollection("dataInterFaceVO").insert( {
     md5: "99deca6d1f7b31ceeeeaf99e5afa488d",
     status: NumberInt("1"),
     _class: "org.cloud.mongo.DataInterFaceVO"
-} );
-
-db.getCollection("dataInterFaceVO").insert( {
-    interfaceName: "授权测试1",
-    interfaceType: "sql",
-    params: [ ],
-    urlOrSqlContent: "授权测试11213321123",
-    microServiceName: "General",
-    authMethod: "BYUSERPERMISSION",
-    createdOrUpdateTime: ISODate("2020-03-15T00:26:18.741Z"),
-    createdOrUpdateBy: NumberLong("1"),
-    createdOrUpdateUserName: "admin",
-    md5: "93714227ad780c1f8c5796943ce3a75c",
-    status: NumberInt("1"),
-    _class: "org.cloud.mongo.DataInterFaceVO"
-} );
-
-db.getCollection("dataInterFaceVO").insert( {
-    interfaceName: "授权测试2",
-    interfaceType: "sql",
-    params: [ ],
-    urlOrSqlContent: "授权测试2授权测试2授权测试2授权测试2授权测试2授权测试2授权测试2授权测试2授权测试2授权测试2授权测试2授权测试2授权测试2",
-    microServiceName: "General",
-    authMethod: "BYUSERPERMISSION",
-    createdOrUpdateTime: ISODate("2020-03-15T00:31:00.139Z"),
-    createdOrUpdateBy: NumberLong("1"),
-    createdOrUpdateUserName: "admin",
-    md5: "5786b12d67c9c42e283d11ccbe0664d7",
-    status: NumberInt("1"),
-    _class: "org.cloud.mongo.DataInterFaceVO"
-} );
-
-db.getCollection("dataInterFaceVO").insert( {
-    interfaceName: "授权测试3",
-    interfaceType: "sql",
-    params: [ ],
-    urlOrSqlContent: "授权测试3授权测试3授权测试3授权测试3授权测试3授权测试3授权测试3授权测试3授权测试3授权测试3授权测试3",
-    microServiceName: "General",
-    authMethod: "BYUSERPERMISSION",
-    createdOrUpdateTime: ISODate("2020-03-15T00:31:14.722Z"),
-    createdOrUpdateBy: NumberLong("1"),
-    createdOrUpdateUserName: "admin",
-    md5: "12c8b95e09760e76a3a81777417cce05",
-    status: NumberInt("1"),
-    _class: "org.cloud.mongo.DataInterFaceVO"
-} );
-
-db.getCollection("dataInterFaceVO").insert( {
-    interfaceName: "interfaceName11213",
-    interfaceType: "sql",
-    params: [ ],
-    urlOrSqlContent: "interfaceName11111111111111",
-    microServiceName: "General",
-    authMethod: "BYUSERPERMISSION",
-    createdOrUpdateTime: ISODate("2020-03-15T00:46:17.724Z"),
-    createdOrUpdateBy: NumberLong("1"),
-    createdOrUpdateUserName: "admin",
-    md5: "fa11bc092628d58f8b2c3fdd91c4b7e6",
-    status: NumberInt("1"),
-    _class: "org.cloud.mongo.DataInterFaceVO"
-} );
-
-db.getCollection("dataInterFaceVO").insert( {
-    interfaceName: "interfaceName12312",
-    interfaceType: "sql",
-    params: [ ],
-    urlOrSqlContent: "interfaceNameinterfaceNameinterfaceNameinterfaceName",
-    microServiceName: "General",
-    authMethod: "BYUSERPERMISSION",
-    createdOrUpdateTime: ISODate("2020-03-15T00:46:32.615Z"),
-    createdOrUpdateBy: NumberLong("1"),
-    createdOrUpdateUserName: "admin",
-    md5: "9afe7bafb7416ccfc17071b1e4514359",
-    status: NumberInt("1"),
-    _class: "org.cloud.mongo.DataInterFaceVO"
-} );
-
-db.getCollection("dataInterFaceVO").insert( {
+}, {
+    _id: ObjectId("5e6f56df0c730000a10008e2"),
     interfaceName: "查询当前角色的数据接口列表",
     interfaceType: "sql",
     params: [ ],
@@ -235,4 +170,61 @@ db.getCollection("dataInterFaceVO").insert( {
     md5: "7d458ef7f373239fd213efa213a30091",
     status: NumberInt("1"),
     _class: "org.cloud.mongo.DataInterFaceVO"
-} );
+}, {
+    _id: ObjectId("5e703d0d1e209d1416226564"),
+    interfaceName: "查询所有用户的接口",
+    interfaceType: "sql",
+    params: [ ],
+    urlOrSqlContent: "select * from t_frame_user\n<where>\n<if test=\"userName!=null\">\nuser_name like concat('%',#{userName,jdbcType=VARCHAR},'%')\n</if>\n<if test=\"status!=null and status.size()>0\">\nand status in\n\t\t  <foreach item=\"statusItem\" collection=\"status\" separator=\",\" open=\"(\" close=\")\" index=\"\">\n\t\t   #{statusItem, jdbcType=INTEGER}\n\t\t </foreach>\n</if>\n</where>",
+    microServiceName: "General",
+    authMethod: "BYUSERPERMISSION",
+    createdOrUpdateTime: ISODate("2020-03-17T03:28:40.161Z"),
+    createdOrUpdateBy: NumberLong("1"),
+    createdOrUpdateUserName: "admin",
+    md5: "3efa00c2c4b06b73758fac9e95e9b6a9",
+    status: NumberInt("1"),
+    _class: "org.cloud.mongo.DataInterFaceVO",
+    restMethod: null
+} ]);
+
+// ----------------------------
+// Function structure for LoggerLevelMapFun
+// ----------------------------
+db.system.js.deleteOne({
+    _id: "LoggerLevelMapFun"
+});
+;
+db.system.js.insert({
+    _id: "LoggerLevelMapFun",
+    value: Code("\r\nfunction() {\r\n    //    for (var idx = 0; idx < this.length; idx++) {\r\n    var key = {\r\n        level: this.level,\r\n        logger: this.logger\r\n    };\r\n\r\n    var value = {\r\n        count: 1,\r\n        createDate: this.createDate\r\n    }\r\n\r\n    emit(key, value);\r\n    //    }\r\n};\r\n")
+})
+;;
+
+
+// ----------------------------
+// Function structure for logerLevelRreduceFun
+// ----------------------------
+db.system.js.deleteOne({
+    _id: "logerLevelRreduceFun"
+});
+;
+db.system.js.insert({
+    _id: "logerLevelRreduceFun",
+    value: Code("function(keyLoggerAndLevel, countObjVals) {\r\n\r\n    var tempMaxDate = new Date(\"9999-01-01\");\r\n    var tempMinDate = new Date(\"1970-01-01\");\r\n\r\n    var reducedVal = {\r\n        count: 0,\r\n        maxCreateDate: null,\r\n        minCreateDate: null\r\n    };\r\n\r\n\r\n    for (var idx = 0; idx < countObjVals.length; idx++) {\r\n        reducedVal.count += countObjVals[idx].count;\r\n\r\n        if (countObjVals[idx].createDate > tempMinDate) {\r\n            tempMinDate = countObjVals[idx].createDate;\r\n        }\r\n\r\n        if (countObjVals[idx].createDate < tempMaxDate) {\r\n            tempMaxDate = countObjVals[idx].createDate;\r\n        }\r\n    }\r\n\r\n    reducedVal.minCreateDate = tempMinDate;\r\n    reducedVal.maxCreateDate = tempMinDate;\r\n\r\n    return reducedVal;\r\n}")
+})
+;;
+
+
+// ----------------------------
+// Function structure for loggerLevelFinalizeFun
+// ----------------------------
+db.system.js.deleteOne({
+    _id: "loggerLevelFinalizeFun"
+});
+;
+db.system.js.insert({
+    _id: "loggerLevelFinalizeFun",
+    value: Code("function(key, reducedVal) {\r\n\t if (reducedVal.createDate != null) {\r\n\t\t\t var newReduceValue={};\r\n\t\t\t newReduceValue.maxCreateDate = reducedVal.createDate;\r\n\t\t\t newReduceValue.minCreateDate = reducedVal.createDate;\r\n\t\t\t newReduceValue.count = reducedVal.count;\r\n\t\t\t return newReduceValue;\r\n\t }\r\n\t \r\n\t return reducedVal;\r\n\r\n};")
+})
+;;
+
