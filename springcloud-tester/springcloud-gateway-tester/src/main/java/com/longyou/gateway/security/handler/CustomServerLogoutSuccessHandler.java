68225b80de3656dafe1ad04d6edcb6fcf2a86e3a
longyou <longyou88888@126.com>
2020-03-28 20:13:10 +0800

    1：退出登录时，清空对应的token

--- springcloud-tester/springcloud-gateway-tester/src/main/java/com/longyou/gateway/security/handler/CustomServerLogoutSuccessHandler.java
+++ springcloud-tester/springcloud-gateway-tester/src/main/java/com/longyou/gateway/security/handler/CustomServerLogoutSuccessHandler.java
@@ -3,6 +3,7 @@ package com.longyou.gateway.security.handler;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.longyou.gateway.security.response.MessageCode;
 import com.longyou.gateway.security.response.WsResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.cloud.constant.CoreConstant;
 import org.cloud.core.redis.RedisUtil;
 import org.cloud.utils.MD5Encoder;
@@ -26,9 +27,10 @@ import reactor.core.publisher.Mono;
 
 @Component
 @Transactional(propagation = Propagation.NEVER)
+@Slf4j
 public class CustomServerLogoutSuccessHandler extends HttpStatusReturningServerLogoutSuccessHandler {
 
-    private final Logger logger = LoggerFactory.getLogger(CustomServerLogoutSuccessHandler.class);
+//    private final Logger logger = LoggerFactory.getLogger(CustomServerLogoutSuccessHandler.class);
     private final HttpStatus httpStatusToReturn=HttpStatus.OK;
     @Autowired
     RedisUtil redisUtil;
@@ -39,7 +41,7 @@ public class CustomServerLogoutSuccessHandler extends HttpStatusReturningServerL
             // 退出时将生成的随机加密值给清空，token直接失效（自动失效时间为24小时），如果想token有效，请不要调用登出接口
             redisUtil.remove(MD5Encoder.encode(webFilterExchange.getExchange().getLogPrefix()));
         } catch (Exception ex) {
-            logger.error(ex.getMessage(), ex);
+            log.error("{}",ex);
         }
         return super.onLogoutSuccess(webFilterExchange,authentication);
     }
