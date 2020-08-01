0f476c0e58d41183fa9a596dedbacaba2df53d67
longyou <longyou88888@126.com>
2020-08-01 16:44:52 +0800

    1：删除不用的modules 2：将token的过期时间默认为120天

--- springcloud-tester/springcloud-gateway-tester/src/main/java/com/longyou/gateway/security/handler/AuthenticationSuccessHandler.java
+++ springcloud-tester/springcloud-gateway-tester/src/main/java/com/longyou/gateway/security/handler/AuthenticationSuccessHandler.java
@@ -72,7 +72,7 @@ public class AuthenticationSuccessHandler extends WebFilterChainServerAuthentica
                 LoginUserDetails loginUserDetails = ((LoginUserDetails) userDetails);
                 loginUserDetails.setToken(token);
                 // 缓存当前登录用户的登录信息
-                redisUtil.set(CoreConstant._BASIC64_TOKEN_USER_CACHE_KEY + MD5Encoder.encode("basic " + token), userDetails, 24 * 60 * 60L);
+                redisUtil.set(CoreConstant._BASIC64_TOKEN_USER_CACHE_KEY + MD5Encoder.encode("basic " + token), userDetails, timeSaltChangeInterval);
                 loginUserDetails.setPassword("***********");
             }
             httpHeaders.add(HttpHeaders.AUTHORIZATION, token);
