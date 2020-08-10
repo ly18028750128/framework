d5cb96a37ad7a6715163ce6db818edcca7f4e1da
longyou <longyou88888@126.com>
2020-08-10 22:27:45 +0800

    增加consul.discovery配置，解决docker注册为本身的ip和端口号的问题，启动命令如下docker run -d --name "commonservice10021" -e JAVA_OPTS="-Dserver.port=10080 \ -Xms512m -Xmx1024m -Xss1024K \ -Dsys.curr_profile=prod \ -Dsystem_config_consul_instance_ip=172.31.70.135 \   指定外部ip -Dsystem_config_consul_instance_port=10021 \  指定外部端口号 -Dsystem.config.consul.host=172.31.70.135 \ -Dsystem.config.consul.port=10050" \ -p 10021:10080 framework/commonservice

--- springcloud-tester/com.longyou.commservice/src/test/java/org/cloud/core/JasyptTest.java
+++ springcloud-tester/com.longyou.commservice/src/test/java/org/cloud/core/JasyptTest.java
@@ -3,11 +3,15 @@ package org.cloud.core;
 import com.longyou.comm.starter.CommonServiceApplication;
 import lombok.extern.slf4j.Slf4j;
 import org.jasypt.encryption.StringEncryptor;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 //@RunWith(SpringRunner.class)
 //
@@ -22,19 +26,29 @@ public class JasyptTest {
 
     @Test
     public void encrypt() throws Exception {
-        String[] yuanWenArr = {"heyichenshijueceRedis@)20", "heyichenshijueceMysql@)20", "heyichenshijueceMongo@)20"
-                , "heyichenshijuecePostgis@)20", "heyichenshijueceGeoserver@)20", "想暴力破解没门",
-                "admin", "root", "postgres", "2f7b0f31bf2d9017e78a2ab78604cb0e"
-        };
-
-//        String[] yuanWenArr = {"1qaz@WSX",
+//        String[] yuanWenArr = {"heyichenshijueceRedis@)20", "heyichenshijueceMysql@)20", "heyichenshijueceMongo@)20"
+//                , "heyichenshijuecePostgis@)20", "heyichenshijueceGeoserver@)20", "想暴力破解没门",
 //                "admin", "root", "postgres", "2f7b0f31bf2d9017e78a2ab78604cb0e"
 //        };
 
-        for (String yuanWen : yuanWenArr) {
-            String encryptStr = stringEncryptor.encrypt(yuanWen);
-            log.info("{}={}", yuanWen, encryptStr);
-            log.info("{}={}", encryptStr, stringEncryptor.decrypt(encryptStr));
+
+        Map<String, String> passwordMap = new LinkedHashMap<>();
+        passwordMap.put("mysql", "isI17^2jM11oP*a2#1qq");
+        passwordMap.put("mongodb", "chuangkekongjian309Mongodb@)20");
+        passwordMap.put("Redis", "chuangkekongjian309Redis@)20");
+        passwordMap.put("consul", "chuangkekongjian309Consul@)20");
+        passwordMap.put("root", "root");
+        passwordMap.put("rcm3pro", "rcm3pro");
+        passwordMap.put("admin", "admin");
+
+
+        for (String key : passwordMap.keySet()) {
+            String encryptStr = stringEncryptor.encrypt(passwordMap.get(key));
+            log.info("{},{}={}", key, passwordMap.get(key), encryptStr);
+
+            Assert.assertEquals(passwordMap.get(key), stringEncryptor.decrypt(encryptStr));
+
+//            log.info("{},{}={}", key, encryptStr, stringEncryptor.decrypt(encryptStr));
         }
     }
 
