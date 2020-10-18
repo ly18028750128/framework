b24b1867601bf26f7e2a37c45692ce1381447787
longyou <longyou88888@126.com>
2020-10-18 15:20:01 +0800

    1：增加后台用登录超时时间的配置，默认为1小时

--- springcloud-tester/springcloud-gateway-tester/src/main/java/com/longyou/gateway/security/handler/AuthenticationSuccessHandler.java
+++ springcloud-tester/springcloud-gateway-tester/src/main/java/com/longyou/gateway/security/handler/AuthenticationSuccessHandler.java
@@ -73,7 +73,7 @@ public class AuthenticationSuccessHandler extends WebFilterChainServerAuthentica
                 // 如果是后台管理用户，那么超时时间为60分钟
                 if (LoginConstants.REGIST_SOURCE_BACKGROUND.equals(loginUserDetails.getUserRegistSource())) {
                     timeSaltChangeInterval = Long.parseLong(CommonUtil.single().getEnv("system.auth_basic_background_expire_time",
-                            Long.toString(24 * 60 * 60L)));
+                            Long.toString(60 * 60L)));
                 }
                 // 缓存当前登录用户的登录信息
                 redisUtil.set(CoreConstant._BASIC64_TOKEN_USER_CACHE_KEY + MD5Encoder.encode("basic " + token), userDetails, timeSaltChangeInterval);
