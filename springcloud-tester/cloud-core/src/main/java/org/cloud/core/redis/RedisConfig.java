cfea3bccf1e2bc8a249133d4ad1cdde581d69e6b
longyou <longyou88888@126.com>
2020-11-26 22:35:50 +0800

    1：删除一些无用的代码

--- springcloud-tester/cloud-core/src/main/java/org/cloud/core/redis/RedisConfig.java
+++ springcloud-tester/cloud-core/src/main/java/org/cloud/core/redis/RedisConfig.java
@@ -64,25 +64,15 @@ public class RedisConfig extends CachingConfigurerSupport {
      */
     @Bean
     public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory lettuceConnectionFactory) {
-        // 设置序列化
-//        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<Object>(Object.class);
-        ObjectMapper om = new ObjectMapper();
-        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
-//        jackson2JsonRedisSerializer.setObjectMapper(om);
         // 配置redisTemplate
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<String, Object>();
         redisTemplate.setConnectionFactory(lettuceConnectionFactory);
         RedisSerializer<?> stringSerializer = new StringRedisSerializer();
         redisTemplate.setKeySerializer(stringSerializer);// key序列化
-//        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);// value序列化
         redisTemplate.setHashKeySerializer(stringSerializer);// Hash key序列化
-//        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);// Hash value序列化
-
         FastJsonRedisSerializer<Object> fastJsonRedisSerializer = new FastJsonRedisSerializer<>(Object.class);
         redisTemplate.setValueSerializer(fastJsonRedisSerializer);// value序列化
         redisTemplate.setHashValueSerializer(fastJsonRedisSerializer);// Hash value序列化
-
         redisTemplate.afterPropertiesSet();
         return redisTemplate;
     }
