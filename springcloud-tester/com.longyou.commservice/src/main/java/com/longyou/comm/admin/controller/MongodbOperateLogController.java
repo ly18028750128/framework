5acf3fbd35afd94029491bff7c3720a594ba284e
xue <1379149253@qq.com>
2020-11-27 18:50:10 +0800

    修改查询日志条件

--- springcloud-tester/com.longyou.commservice/src/main/java/com/longyou/comm/admin/controller/MongodbOperateLogController.java
+++ springcloud-tester/com.longyou.commservice/src/main/java/com/longyou/comm/admin/controller/MongodbOperateLogController.java
@@ -6,6 +6,7 @@ import org.cloud.constant.CoreConstant;
 import org.cloud.vo.ResponseResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
@@ -13,8 +14,13 @@ import org.springframework.http.MediaType;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 @RestController
 @RequestMapping(value = "/admin/mongo/OperateLog", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -33,18 +39,35 @@ public class MongodbOperateLogController {
             return result;
         }
         String collectionName = appGroup + queryParams.get("microName") + CoreConstant.MongoDbLogConfig.MONGODB_OPERATE_LOG_SUFFIX.value();
-        queryParams.remove("microName");
-        Query query = new Query();
-        //遍历查询条件并添加条件
-        queryParams.forEach((key, value) -> {
-            if (key.equals("startTime")){
-                query.addCriteria(Criteria.where(key).gte(value));
-            }else if (key.equals("endTime")){
+        Criteria criteria = new Criteria();
+        if (!StringUtils.isEmpty(queryParams.get("userName"))){
+            Pattern pattern= Pattern.compile("^.*"+ queryParams.get("userName") +".*$");
+            criteria.and("userName").regex(pattern);
+        }
+        if (!StringUtils.isEmpty(queryParams.get("type"))){
+            criteria.and("type").is(Integer.parseInt(String.valueOf(queryParams.get("type"))));
+        }
+        if (!StringUtils.isEmpty(queryParams.get("desc"))){
+            Pattern pattern= Pattern.compile("^.*"+ queryParams.get("desc") +".*$");
 
-            }else {
-                query.addCriteria(Criteria.where(key).lt(value));
-            }
-        });
+            criteria.orOperator(Criteria.where("resMsg").regex(pattern),
+                    Criteria.where("uri").regex(pattern),
+                    Criteria.where("reqIp").regex(pattern),
+                    Criteria.where("params").regex(pattern),
+                    Criteria.where("desc").regex(pattern));
+        }
+        if (!StringUtils.isEmpty(queryParams.get("startTime"))){
+            criteria.and("createDate").gte(strToDateLong(queryParams.get("startTime").toString()));
+        }
+        if (!StringUtils.isEmpty(queryParams.get("endTime"))){
+            criteria.and("createDate").lt(strToDateLong(queryParams.get("endTime").toString()));
+        }
+        Query query = new Query(criteria);
+        //排序
+        Sort.Order createTimeDesc = new Sort.Order(Sort.Direction.DESC, "createDate");
+        ArrayList<Sort.Order> orders = new ArrayList<>();
+        orders.add(createTimeDesc);
+        query.with(Sort.by(orders));
         //分页查询数据
         List<Map> listData = mongoTemplate.find(query.skip((page - 1) * pageSize).limit(pageSize), Map.class, collectionName);
         PageInfo pageInfo = new PageInfo(listData);
@@ -54,5 +77,16 @@ public class MongodbOperateLogController {
         pageInfo.setTotal(mongoTemplate.getCollection(collectionName).countDocuments(query.getQueryObject()));
         result.setData(pageInfo);
         return result;
+    }
+
+    public static Date strToDateLong(String strDate) {
+        Date strtodate = null;
+        try {
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            strtodate = formatter.parse(strDate);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return strtodate;
     }
 }
