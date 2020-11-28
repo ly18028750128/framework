839875bc7952443ad0ed2fae895aae01c92c3a71
longyou <longyou88888@126.com>
2020-11-28 11:12:00 +0800

    1：增加redisson集群配置

--- springcloud-tester/cloud-core/src/main/java/org/cloud/config/redisson/RedissonConfig.java
+++ springcloud-tester/cloud-core/src/main/java/org/cloud/config/redisson/RedissonConfig.java
@@ -7,6 +7,7 @@ import lombok.Data;
 import org.cloud.utils.RedissonUtil;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
+import org.redisson.config.ClusterServersConfig;
 import org.redisson.config.Config;
 import org.redisson.config.MasterSlaveServersConfig;
 import org.springframework.beans.BeanUtils;
@@ -14,6 +15,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.Assert;
 
 @ConditionalOnProperty(prefix = "system.redisson", name = "enabled", havingValue = "true")
 @ConfigurationProperties(prefix = "system.redisson")
@@ -21,16 +23,28 @@ import org.springframework.context.annotation.Configuration;
 @Data
 public class RedissonConfig extends Config {
 
-    MasterSlaveServersConfig masterSlaveConfig;
+    // 支持如下配置，优先级为，1：主从配置 2：集群配置
+    MasterSlaveServersConfig masterSlaveConfig; // 主从配置
+    ClusterServersConfig clusterConfig; // 集群配置
 
     @Bean
     public RedissonClient RedissonClient() {
+        Assert.isTrue(masterSlaveConfig != null || clusterConfig != null, "必须配置一个相关的redis配置，请检查");
+        if (masterSlaveConfig != null) {
+            MasterSlaveServersConfig masterSlaveServersConfig = this.useMasterSlaveServers();
+            BeanUtils.copyProperties(masterSlaveConfig, masterSlaveServersConfig);
+        } else if (clusterConfig != null) {
+            ClusterServersConfig clusterServersConfig = this.useClusterServers();
+            clusterServersConfig.getNodeAddresses().addAll(clusterConfig.getNodeAddresses());
+            BeanUtils.copyProperties(clusterConfig, clusterServersConfig);
+        } else {
+
+        }
         System.setProperty("fastjson.parser.autoTypeSupport", "true");
-        MasterSlaveServersConfig masterSlaveServersConfig = this.useMasterSlaveServers();
-        BeanUtils.copyProperties(masterSlaveConfig, masterSlaveServersConfig);
         this.setCodec(new FastjsonCodec());
         return Redisson.create(this);
     }
+
 
     @Bean
     public RedissonUtil initRedissonUtil(RedissonClient redissonClient) {
