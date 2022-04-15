bc810c523039c161d5aa799cd2e7cacc8c5893d9
longyou <ly18028750128@163.com>
2022-04-15 18:46:59 +0800

    1：为了兼容手机钱包，将验证签名的方式更改为this.web3.eth.personal.sign，并在后台解开，代码示例参考wtb前台

--- springcloud-tester/com.longyou.commservice/src/main/java/com/longyou/comm/service/impl/EthChainLoginUserGetService.java
+++ springcloud-tester/com.longyou.commservice/src/main/java/com/longyou/comm/service/impl/EthChainLoginUserGetService.java
@@ -5,6 +5,8 @@ import com.longyou.comm.config.MicroAppConfigList;
 import com.longyou.comm.mapper.UserInfoMapper;
 import com.longyou.comm.service.IUserInfoService;
 import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.SignatureException;
 import java.util.Locale;
 import lombok.extern.slf4j.Slf4j;
 import org.bouncycastle.util.Arrays;
@@ -16,6 +18,7 @@ import org.cloud.vo.LoginUserGetParamsDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 import org.web3j.crypto.ECDSASignature;
 import org.web3j.crypto.Hash;
 import org.web3j.crypto.Keys;
@@ -50,6 +53,7 @@ public class EthChainLoginUserGetService implements LoginUserGetInterface {
     RedisUtil redisUtil;
 
     public static final String ADDRESS_SIGN_MAP_KEY = "system:config:ETH-CHAIN-SIGN-MAP:";  // 签名数据存放的map
+    private static final String MESSAGE_PREFIX = "\u0019Ethereum Signed Message:\n";
 
 
     @Override
@@ -57,60 +61,31 @@ public class EthChainLoginUserGetService implements LoginUserGetInterface {
 
         // 先对password进行验证操作
         // 解密用户名，需要提供加密接口，暂定用rsa加密，防止别人抓包后重新登录
-
         final String address = loginUserGetParamsDTO.getUserName();
-
         final String addressSignKey = ADDRESS_SIGN_MAP_KEY + address;
-
         loginUserGetParamsDTO.setUserName(address.toLowerCase(Locale.ROOT));
-
         final String message = loginUserGetParamsDTO.getParamMap().get("signValue").toString();
-
         Long expireEndTime = redisUtil.hashGet(addressSignKey, message);
         if (expireEndTime != null && System.currentTimeMillis() > expireEndTime) {
             throw new BusinessException("签名数据已经过期,请重新签名！");
         }
-
-        byte[] msgHash = Hash.sha3((message).getBytes());
+        String prefix = MESSAGE_PREFIX + message.length();
+        byte[] msgHash = (prefix + message).getBytes(StandardCharsets.UTF_8);
         byte[] signatureBytes = Numeric.hexStringToByteArray(loginUserGetParamsDTO.getPassword());
         byte v = signatureBytes[64];
         if (v < 27) {
             v += 27;
         }
-
         SignatureData sd =
             new SignatureData(
                 v,
                 Arrays.copyOfRange(signatureBytes, 0, 32),
                 Arrays.copyOfRange(signatureBytes, 32, 64));
 
-        String addressRecovered = null;
-        boolean match = false;
-
-        // Iterate for each possible key to recover
-        for (int i = 0; i < 4; i++) {
-            BigInteger publicKey =
-                Sign.recoverFromSignature(
-                    (byte) i,
-                    new ECDSASignature(
-                        new BigInteger(1, sd.getR()), new BigInteger(1, sd.getS())),
-                    msgHash);
-
-            if (publicKey != null) {
-                addressRecovered = "0x" + Keys.getAddress(publicKey);
-                if (addressRecovered.equalsIgnoreCase(address)) {
-                    match = true;
-                    break;
-                }
-            }
-        }
-
-        if (!match) {
-            throw new BusinessException("签名数据验证错误！");
-        }
-
+        BigInteger recoveredKey = Sign.signedMessageToKey(msgHash, sd);
+        String addressRecovered = "0x" + Keys.getAddress(recoveredKey);
+        Assert.isTrue(addressRecovered.equalsIgnoreCase(address), "address error");
         redisUtil.hashSet(addressSignKey, message, System.currentTimeMillis() + signValueExpireTime, 3600L);
-
         return LoginUtils.createOrUpdateUserByLoginUserGetParamsDTO(loginUserGetParamsDTO, salt);
 
     }
