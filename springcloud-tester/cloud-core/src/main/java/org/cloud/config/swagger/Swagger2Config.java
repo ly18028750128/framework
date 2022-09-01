828cef456efddeff52e106f2035a2ff18329629b
ly18028750128 <87405411+ly18028750128@users.noreply.github.com>
2022-09-01 15:16:06 +0800

    1：增加swagger的可配置选项，相关选项参考org/cloud/config/swagger/Swagger2Config.java:33

--- springcloud-tester/cloud-core/src/main/java/org/cloud/config/swagger/Swagger2Config.java
+++ springcloud-tester/cloud-core/src/main/java/org/cloud/config/swagger/Swagger2Config.java
@@ -1,5 +1,6 @@
 package org.cloud.config.swagger;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,26 +23,53 @@ import springfox.documentation.spring.web.plugins.Docket;
 @EnableOpenApi
 public class Swagger2Config {
 
+    // 默认打开，生产环境需要关闭
+    @Value("${swagger.enabled:true}")
+    private Boolean enabled;
+
+    @Value("${swagger.title:链自由 Restful Api}")
+    private String title;
+
+    @Value("${swagger.description:链自由接口描述}")
+    private String description;
+
+
+    @Value("${swagger.version:1.0}")
+    private String version;
+
+    @Value("${swagger.serverUrl:http://127.0.0.1}")
+    private String serverUrl;
+
+    @Value("${swagger.contact.name:链自由}")
+    private String contactName;
+
+    @Value("${swagger.contact.url:http://127.0.0.1}")
+    private String contactUrl;
+
+    @Value("${swagger.contact.email:test.qq.com}")
+    private String contactEmail;
+
+
     @Bean
     public Docket createRestApi() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(this.apiInfo())
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
+            .enable(enabled)
+            .apiInfo(this.apiInfo())
+            .select()
+            .apis(RequestHandlerSelectors.any())
+            .paths(PathSelectors.any())
+            .build();
     }
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("链自由 Restful Api")
-                .description("链自由接口描述")
-                .termsOfServiceUrl("http://127.0.0.1")
-                .contact(new Contact("lianziyou", "", ""))
-                .version("3.0")
-                .build();
+            .title(title)
+            .description(description)
+            .termsOfServiceUrl(serverUrl)
+            .contact(new Contact(contactName, contactUrl, contactEmail))
+            .version(version)
+            .build();
     }
-
 
 
 }
