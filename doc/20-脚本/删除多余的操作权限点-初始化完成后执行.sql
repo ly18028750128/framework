66c36fa5444bac4e6cba7d8b5a2098392d2a64bc
--remote <87405411+ly18028750128@users.noreply.github.com>
2022-09-16 18:26:02 +0800

    1：增加删除多余的权限点的脚本

---
+++ doc/20-脚本/删除多余的操作权限点-初始化完成后执行.sql
@@ -0,0 +1,8 @@
+
+delete from t_frame_role_resource where t_frame_role_resource.resource_id in(
+
+    select t_framework_resource.resource_id from t_framework_resource where belong_microservice not in('common-service','spring-gateway','chain-block-scan-service')
+);
+
+
+delete from t_framework_resource where belong_microservice not in('common-service','spring-gateway','chain-block-scan-service')
