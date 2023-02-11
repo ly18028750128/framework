6c0050fda37b195f2639028a3a60036afc8a19af
--remote <87405411+ly18028750128@users.noreply.github.com>
2023-02-11 09:39:35 +0800

    1：增加异步导入导出的实现

--- com.unknow.first.common/export-import-api/doc/init.sql
+++ com.unknow.first.common/export-import-api/doc/init.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80030 (8.0.30)
  File Encoding         : 65001
 
- Date: 09/02/2023 19:18:58
+ Date: 11/02/2023 09:38:51
 */
 
 SET NAMES utf8mb4;
@@ -23,7 +23,7 @@ SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `t_frame_export_template`;
 CREATE TABLE `t_frame_export_template`  (
   `template_id` int NOT NULL AUTO_INCREMENT,
-  `template_code` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '模板编码',
+  `template_code` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '模板编码',
   `template_type` tinyint NOT NULL DEFAULT 10 COMMENT '模板类型：10(excel),20(pdf),30(word)',
   `file_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '文件ID，存储在mongodb里的文件ObjectId',
   `status` tinyint NOT NULL DEFAULT 1 COMMENT '状态，1/有效 0/无效',
@@ -44,6 +44,7 @@ DROP TABLE IF EXISTS `t_frame_import_export_task`;
 CREATE TABLE `t_frame_import_export_task`  (
   `task_id` bigint NOT NULL AUTO_INCREMENT,
   `task_type` tinyint NOT NULL COMMENT '任务类型：1(导入) 2(导出)',
+  `task_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '任务名称',
   `file_name` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '导入或者导出的文件名',
   `file_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '文件id，用于存储导入或者导出的文件objectId',
   `error_file_name` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '导入或者导出出错时保存错误的文件名',
@@ -53,19 +54,20 @@ CREATE TABLE `t_frame_import_export_task`  (
   `data_correct_count` bigint NOT NULL DEFAULT 0 COMMENT '正确行数',
   `start_time` datetime NULL DEFAULT NULL COMMENT '开始执行时间',
   `end_time` datetime NULL DEFAULT NULL COMMENT '结束执行时间',
+  `execute_seconds` bigint GENERATED ALWAYS AS (timestampdiff(SECOND,`start_time`,`end_time`)) STORED NULL,
   `message` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '返回消息',
+  `template_code` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '模板编码',
   `task_status` tinyint NOT NULL DEFAULT 1 COMMENT '任务状态：1(未执行) 2(执行中)3(执行成功)-1(执行失败)',
+  `belong_microservice` varchar(200) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT 'common' COMMENT '所属微服务',
+  `process_class` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '线程执行类的名称，导出继承ImportCallableService，导出继承ExportCallableService',
+  `params` json NULL COMMENT '任务参数',
   `create_by` bigint NOT NULL COMMENT '创建人ID',
   `create_by_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '创建人名称',
   `create_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建日期',
   `update_by` bigint NOT NULL COMMENT '更新人ID',
   `update_by_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '创建人名称',
   `update_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新日期',
-  `execute_seconds` bigint GENERATED ALWAYS AS (timestampdiff(SECOND,`start_time`,`end_time`)) STORED NULL,
-  `belong_microservice` varchar(200) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT 'common' COMMENT '所属微服务',
-  `process_class` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '线程执行类的名称，继承ImexportRunnableService',
-  `params` json NULL COMMENT '任务参数',
   PRIMARY KEY (`task_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 32 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
