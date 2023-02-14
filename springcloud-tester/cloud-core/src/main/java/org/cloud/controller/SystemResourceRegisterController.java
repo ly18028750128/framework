abcce59191c55468e9a261556f674e5c88b40c1a
xuehongyang <1379149253@qq.com>
2023-02-14 16:35:48 +0800

    注册操作资源到数据库时的ComponentPath修改为layout/routerView/parent

--- springcloud-tester/cloud-core/src/main/java/org/cloud/controller/SystemResourceRegisterController.java
+++ springcloud-tester/cloud-core/src/main/java/org/cloud/controller/SystemResourceRegisterController.java
@@ -3,9 +3,6 @@ package org.cloud.controller;
 import com.alibaba.fastjson.JSON;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import java.lang.reflect.Method;
-import java.util.Date;
-import java.util.Map;
 import org.cloud.annotation.SystemResource;
 import org.cloud.constant.CoreConstant;
 import org.cloud.constant.CoreConstant.AuthMethod;
@@ -26,6 +23,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.lang.reflect.Method;
+import java.util.Date;
+import java.util.Map;
 
 @Api(value = "SystemResourceRegisterController", tags = "系统资源注册接口")
 @RestController
@@ -83,7 +84,8 @@ public class SystemResourceRegisterController {
                 parentMenu.setUpdateDate(new Date());
                 parentMenu.setType(0);  // 类上注解定义为目录
                 parentMenu.setShowType(0);
-                parentMenu.setComponentPath("/layout/Layout");
+//                parentMenu.setComponentPath("/layout/Layout");    //旧的后台管理
+                parentMenu.setComponentPath("layout/routerView/parent");    //新的后台管理
                 parentMenu.setMenuUrl(beanResourceAnnotation.path());
                 parentMenu.setStatus(1);
                 try {
