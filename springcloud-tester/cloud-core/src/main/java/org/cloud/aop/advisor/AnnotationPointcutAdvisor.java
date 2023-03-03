8a9b3b968d15935184ec44485812876d45eeffca
--remote <87405411+ly18028750128@users.noreply.github.com>
2023-03-03 17:05:36 +0800

    1：提交定时任务日志相关代码

---
+++ springcloud-tester/cloud-core/src/main/java/org/cloud/aop/advisor/AnnotationPointcutAdvisor.java
@@ -0,0 +1,46 @@
+package org.cloud.aop.advisor;
+
+import org.aopalliance.aop.Advice;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.springframework.aop.Pointcut;
+import org.springframework.aop.support.AbstractPointcutAdvisor;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+
+/**
+ * @program: spring-parent
+ * @description: 切面增强类，增强类=切点+切面（拦截器advice）
+ * @author: Emily
+ * @create: 2022/01/12
+ */
+public class AnnotationPointcutAdvisor extends AbstractPointcutAdvisor implements BeanFactoryAware {
+
+    private final Advice advice;
+
+    private final Pointcut pointcut;
+
+    public AnnotationPointcutAdvisor(MethodInterceptor interceptor, Pointcut pointcut) {
+        this.advice = interceptor;
+        this.pointcut = pointcut;
+    }
+
+    @Override
+    public Pointcut getPointcut() {
+        return this.pointcut;
+    }
+
+    @Override
+    public Advice getAdvice() {
+        return this.advice;
+    }
+
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        if (this.advice instanceof BeanFactoryAware) {
+            ((BeanFactoryAware) this.advice).setBeanFactory(beanFactory);
+        }
+    }
+
+}
---
+++ springcloud-tester/com.longyou.commservice/src/main/java/com/longyou/comm/conntroller/admin/JobLogAdminController.java
@@ -0,0 +1,40 @@
+package com.longyou.comm.conntroller.admin;
+
+import static com.longyou.comm.CommonMenuConst.MENU_JOB_LOG_MANAGER;
+import static com.longyou.comm.CommonMenuConst.MENU_MESSAGE_LOG_MANAGER;
+import static com.longyou.comm.CommonMenuConst.MENU_SYSTEM_MANAGER;
+import static org.cloud.scheduler.interceptor.JobTaskLogCustomizer.JOB_LOGS_COLLECTION;
+
+import com.github.pagehelper.PageInfo;
+import com.longyou.comm.dto.JobLogQueryDTO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.cloud.annotation.SystemResource;
+import org.cloud.constant.CoreConstant;
+import org.cloud.mongo.MongoPagedParam;
+import org.cloud.mongo.MongoQueryParamsDTO;
+import org.cloud.scheduler.dto.JobTaskLog;
+import org.cloud.utils.mongo.MongoDBUtil;
+import org.cloud.vo.CommonApiResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@Api(tags = "管理员:定时任务日志管理", value = "管理员:定时任务日志管理")
+@RequestMapping("/admin/job/logs")
+@SystemResource(path = "/admin/job/logs", parentMenuCode = MENU_SYSTEM_MANAGER, parentMenuName = "系统管理")
+@Slf4j
+public class JobLogAdminController {
+    @SystemResource(value = "/list", description = "管理员查询消息日志", authMethod = CoreConstant.AuthMethod.BYUSERPERMISSION, menuName = "定时任务日志", menuCode = MENU_JOB_LOG_MANAGER)
+    @GetMapping
+    @ApiOperation("管理员查询定时任务日志")
+    public CommonApiResult<PageInfo<JobTaskLog>> list(JobLogQueryDTO jobLogQueryDTO, MongoPagedParam mongoPagedParam) throws Exception {
+        MongoQueryParamsDTO mongoQueryParamsDTO = MongoDBUtil.single().buildQueryParamsDTO(jobLogQueryDTO, mongoPagedParam);
+        PageInfo<JobTaskLog> result = MongoDBUtil.single()
+            .paged(mongoPagedParam.getPage().longValue(), mongoPagedParam.getLimit().longValue(), mongoQueryParamsDTO, JobTaskLog.class, JOB_LOGS_COLLECTION);
+        return CommonApiResult.createSuccessResult(result);
+    }
+}
