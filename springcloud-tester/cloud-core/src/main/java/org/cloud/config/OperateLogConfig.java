d7c7ba3bf72be4e823e8b6612f7d9f4455cfa241
--remote <87405411+ly18028750128@users.noreply.github.com>
2023-03-13 14:47:19 +0800

    重新整理定时任务的脚本

--- springcloud-tester/cloud-core/src/main/java/org/cloud/config/OperateLogConfig.java
+++ springcloud-tester/cloud-core/src/main/java/org/cloud/config/OperateLogConfig.java
@@ -1,23 +1,13 @@
 package org.cloud.config;
 
-import lombok.Getter;
-import lombok.Setter;
 import org.cloud.aop.OperateLogAspect;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConditionalOnProperty(prefix = "system.OperateLog", name = "enabled", havingValue = "true")
+@ConditionalOnProperty(prefix = "system.OperateLog", name = "enabled", matchIfMissing = true)
 public class OperateLogConfig {
-
-  @Setter
-  @Getter
-  private String key;
-  @Setter
-  @Getter
-  private String vi;
-
   @Bean
   public OperateLogAspect operateLogAspect() {
     return new OperateLogAspect();
