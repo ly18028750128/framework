9f7b428698ed6a9286951bf8feaf4ecb42c80ba3
--remote <87405411+ly18028750128@users.noreply.github.com>
2023-05-25 19:41:36 +0800

    1:增加椭圆曲线加密的util

--- springcloud-tester/cloud-core/src/main/java/org/cloud/utils/EccUtil.java
+++ springcloud-tester/cloud-core/src/main/java/org/cloud/utils/EccUtil.java
@@ -3,15 +3,16 @@ package org.cloud.utils;
 import static javax.crypto.Cipher.DECRYPT_MODE;
 import static javax.crypto.Cipher.ENCRYPT_MODE;
 
-import java.math.BigInteger;
 import java.security.KeyFactory;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.security.SecureRandom;
 import java.security.Security;
 import java.security.Signature;
-import java.security.spec.ECGenParameterSpec;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.ArrayList;
 import java.util.Base64;
 import java.util.List;
@@ -19,8 +20,6 @@ import javax.crypto.Cipher;
 import org.bouncycastle.jce.ECNamedCurveTable;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.jce.spec.ECParameterSpec;
-import org.bouncycastle.jce.spec.ECPrivateKeySpec;
-import org.bouncycastle.jce.spec.ECPublicKeySpec;
 import org.cloud.core.redis.RedisUtil;
 
 public final class EccUtil {
@@ -35,7 +34,7 @@ public final class EccUtil {
         return instance;
     }
 
-    private static final String ALGORITHM = "EC";
+    private static final String ALGORITHM = "ECIES";
     private static final String PROVIDER = "BC";
 
     private static final String CURVE_NAME = "secp256k1";
@@ -48,14 +47,19 @@ public final class EccUtil {
      * @throws Exception
      */
     public List<String> getEccKey() throws Exception {
-        KeyPairGenerator keyGen = KeyPairGenerator.getInstance(ALGORITHM, PROVIDER);
-        ECGenParameterSpec ecSpec = new ECGenParameterSpec(CURVE_NAME);
-        keyGen.initialize(ecSpec);
-        KeyPair keyPair = keyGen.generateKeyPair();
+        KeyPair keyPair = getKeyPair();
         List<String> keys = new ArrayList<>();
         keys.add(Base64.getEncoder().encodeToString(keyPair.getPublic().getEncoded()));
         keys.add(Base64.getEncoder().encodeToString(keyPair.getPrivate().getEncoded()));
         return keys;
+    }
+
+    public KeyPair getKeyPair() throws Exception {
+        KeyPairGenerator keyGen = KeyPairGenerator.getInstance(ALGORITHM, PROVIDER);
+        ECParameterSpec ecSpec = ECNamedCurveTable.getParameterSpec(CURVE_NAME);
+        keyGen.initialize(ecSpec, new SecureRandom());
+        return keyGen.generateKeyPair();
+
     }
 
     public List<String> getKeyFromRedis() throws Exception {
@@ -77,7 +81,7 @@ public final class EccUtil {
     }
 
     public String encrypt(String str, String publicKeyStr) throws Exception {
-        return new String(this.encrypt(str.getBytes("UTF-8"), convertStringToPublicKey(publicKeyStr)));
+        return Base64.getEncoder().encodeToString(this.encrypt(str.getBytes("UTF-8"), convertStringToPublicKey(publicKeyStr)));
     }
 
     public String decryptFromRedis(String str) throws Exception {
@@ -91,24 +95,23 @@ public final class EccUtil {
     }
 
     public String decrypt(String str, String privateKeyStr) throws Exception {
-        byte[] inputByte = Base64.getDecoder().decode(str.getBytes("UTF-8"));
-        return new String(decrypt(inputByte, convertStringToPrivateKey(privateKeyStr)));
+        return new String(decrypt(Base64.getDecoder().decode(str), convertStringToPrivateKey(privateKeyStr)));
     }
+
 
     public String signFromRedis(String str) throws Exception {
         return this.sign(str, getKeyFromRedis().get(1));
     }
 
     public byte[] sign(byte[] data, PrivateKey privateKey) throws Exception {
-        Signature signatureAlgorithm = Signature.getInstance(ALGORITHM, PROVIDER);
+        Signature signatureAlgorithm = Signature.getInstance("SHA256withECDSA", PROVIDER);
         signatureAlgorithm.initSign(privateKey);
         signatureAlgorithm.update(data);
         return signatureAlgorithm.sign();
     }
 
-    public String sign(String str, String publicKey) throws Exception {
-        byte[] inputByte = Base64.getDecoder().decode(str.getBytes("UTF-8"));
-        return new String(sign(inputByte, convertStringToPrivateKey(publicKey)));
+    public String sign(String str, String privateKeyStr) throws Exception {
+        return Base64.getEncoder().encodeToString(sign(str.getBytes(), convertStringToPrivateKey(privateKeyStr)));
     }
 
     public boolean verifySignatureFromRedis(String str, String signature) throws Exception {
@@ -116,15 +119,14 @@ public final class EccUtil {
     }
 
     public static boolean verifySignature(byte[] data, byte[] signature, PublicKey publicKey) throws Exception {
-        Signature signatureAlgorithm = Signature.getInstance(ALGORITHM, PROVIDER);
+        Signature signatureAlgorithm = Signature.getInstance("SHA256withECDSA", PROVIDER);
         signatureAlgorithm.initVerify(publicKey);
         signatureAlgorithm.update(data);
         return signatureAlgorithm.verify(signature);
     }
 
     public boolean verifySignature(String str, String signature, String publicKey) throws Exception {
-        byte[] inputByte = Base64.getDecoder().decode(str.getBytes("UTF-8"));
-        return verifySignature(inputByte, signature.getBytes("UTF-8"), convertStringToPublicKey(publicKey));
+        return verifySignature(str.getBytes(), Base64.getDecoder().decode(signature), convertStringToPublicKey(publicKey));
     }
 
 
@@ -132,20 +134,21 @@ public final class EccUtil {
         return convertBytesToPublicKey(Base64.getDecoder().decode(publicKeyString));
     }
 
-    private PublicKey convertBytesToPublicKey(byte[] publicKeyBytes) throws Exception {
-        ECParameterSpec ecSpec = ECNamedCurveTable.getParameterSpec(CURVE_NAME); // 使用secp256k1曲线
-        ECPublicKeySpec pubKeySpec = new ECPublicKeySpec(ecSpec.getCurve().decodePoint(publicKeyBytes), ecSpec);
-        KeyFactory keyFactory = KeyFactory.getInstance(ALGORITHM, PROVIDER);
-        return keyFactory.generatePublic(pubKeySpec);
+    public PublicKey convertBytesToPublicKey(byte[] publicKeyBytes) throws Exception {
+        X509EncodedKeySpec bobPubKeySpec = new X509EncodedKeySpec(publicKeyBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance("EC");
+        return keyFactory.generatePublic(bobPubKeySpec);
     }
+
 
     public PrivateKey convertStringToPrivateKey(String privateKeyString) throws Exception {
-        ECParameterSpec ecSpec = ECNamedCurveTable.getParameterSpec(CURVE_NAME); // 使用secp256k1曲线
-        ECPrivateKeySpec privateKeySpec = new ECPrivateKeySpec(new BigInteger(1, Base64.getDecoder().decode(privateKeyString)), ecSpec);
-        KeyFactory keyFactory = KeyFactory.getInstance(ALGORITHM, PROVIDER);
-        return keyFactory.generatePrivate(privateKeySpec);
+        return convertStringToPrivateKey(Base64.getDecoder().decode(privateKeyString));
     }
-
+    public PrivateKey convertStringToPrivateKey(byte[] privateKeyBytes) throws Exception {
+        PKCS8EncodedKeySpec pkcs8EncodedKeySpec = new PKCS8EncodedKeySpec(privateKeyBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance("EC");
+        return keyFactory.generatePrivate(pkcs8EncodedKeySpec);
+    }
 
 
 }
--- springcloud-tester/cloud-core/src/test/java/org/cloud/utils/EccUtilTest.java
+++ springcloud-tester/cloud-core/src/test/java/org/cloud/utils/EccUtilTest.java
@@ -1,8 +1,6 @@
 package org.cloud.utils;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import java.util.ArrayList;
+import java.security.KeyPair;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -13,18 +11,22 @@ class EccUtilTest {
     @Test
     void single() throws Exception {
 
-        EccUtil eccUtil =  EccUtil.single();
+        EccUtil eccUtil = EccUtil.single();
 
         List<String> keys = eccUtil.getEccKey();
 
-
-        log.info("keys:{}",keys);
+        log.info("keys:{}", keys);
 
         String str = "这里是中国";
 
+        String encStr = eccUtil.encrypt(str, keys.get(0));
+        log.info("加密:{}", encStr);
+        log.info("解密:{}", eccUtil.decrypt(encStr, keys.get(1)));
 
-        eccUtil.encrypt(str,keys.get(0));
 
+        String signStr = eccUtil.sign(str, keys.get(1));
+        log.info("签名:{}", signStr);
 
+        log.info("签名验证:{}", eccUtil.verifySignature(str,signStr,keys.get(0)));
     }
 }
