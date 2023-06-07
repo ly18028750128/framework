63f1e285c754961b532b9ae7a8d2fc9fe616fa94
--remote <87405411+ly18028750128@users.noreply.github.com>
2023-06-07 15:34:48 +0800

    1:更改机器人消息的发送方式

--- com.unknow.first.common/telegram-message/src/main/java/com/unkow/first/telegram/TelegramBotsConfig.java
+++ com.unknow.first.common/telegram-message/src/main/java/com/unkow/first/telegram/TelegramBotsConfig.java
@@ -1,6 +1,8 @@
 package com.unkow.first.telegram;
 
+import java.util.LinkedHashSet;
 import java.util.Map;
+import java.util.Set;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
@@ -14,39 +16,44 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 @Configuration
 @Slf4j
 public class TelegramBotsConfig implements BeanFactoryPostProcessor {
+
     /**
      * Spring应用上下文环境
      */
     private static ConfigurableListableBeanFactory beanFactory;
-    @Bean
-    public TelegramBotsApi telegramBotsApi() {
 
-        TelegramBotsApi telegramBotsApi;
-        try {
-            telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+    private static Set<String> registerBots = new LinkedHashSet<>();
 
-            Map<String,MyBot> myBots = beanFactory.getBeansOfType(MyBot.class);
-
-            if(!myBots.isEmpty()){
-                myBots.values().forEach(myBot -> {
-                    try {
-                        telegramBotsApi.registerBot(myBot);
-                    } catch (TelegramApiException e) {
-                        log.error("{}注册失败",myBot.getBotUsername());
-                    }
-                });
-            }
-        } catch (TelegramApiException e) {
-            return null;
-        }
-        return telegramBotsApi;
-    }
+//    @Bean
+//    public TelegramBotsApi telegramBotsApi() {
+//
+//        TelegramBotsApi telegramBotsApi;
+//        try {
+//            telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+//            Map<String, MyBot> myBots = beanFactory.getBeansOfType(MyBot.class);
+//            if (!myBots.isEmpty()) {
+//                myBots.values().forEach(myBot -> {
+//                    try {
+//                        if (registerBots.contains(myBot.getBotToken())) {
+//                            return;
+//                        }
+//                        telegramBotsApi.registerBot(myBot);
+//                        registerBots.add(myBot.getBotToken());
+//                    } catch (TelegramApiException e) {
+//                        log.error("{}注册失败", myBot.getBotUsername());
+//                    }
+//                });
+//            }
+//        } catch (TelegramApiException e) {
+//            return null;
+//        }
+//        return telegramBotsApi;
+//    }
 
     @Override
     public void postProcessBeanFactory(ConfigurableListableBeanFactory configurableListableBeanFactory) throws BeansException {
         TelegramBotsConfig.beanFactory = configurableListableBeanFactory;
     }
-
 
 
 }
