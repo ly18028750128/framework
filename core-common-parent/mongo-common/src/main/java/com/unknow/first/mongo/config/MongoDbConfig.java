f8706d3c60941b9b58149029d08f222edf5a44f5
youyoulong <93334958@qq.com>
2023-08-10 17:53:07 +0800

    1:提交新的接口配置脚本，菜单授权查询权限点只查询出要授权的权限点

--- core-common-parent/mongo-common/src/main/java/com/unknow/first/mongo/config/MongoDbConfig.java
+++ core-common-parent/mongo-common/src/main/java/com/unknow/first/mongo/config/MongoDbConfig.java
@@ -1,11 +1,20 @@
 package com.unknow.first.mongo.config;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.bean.copier.CopyOptions;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoCredential;
 import com.unknow.first.mongo.utils.MongoDBUtil;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoClientFactoryBean;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.gridfs.GridFsTemplate;
+
+import static com.mongodb.connection.SocketSettings.builder;
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 /**
  * @author Administrator
@@ -16,6 +25,6 @@ public class MongoDbConfig {
 
     @Bean
     public MongoDBUtil mongoDBUtil(MongoTemplate mongoTemplate, GridFsTemplate gridFsTemplate) {
-        return new MongoDBUtil(mongoTemplate,gridFsTemplate);
+        return new MongoDBUtil(mongoTemplate, gridFsTemplate);
     }
 }
