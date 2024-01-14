42144966cf95b8dc92e3e9bcdeebe6dcc97726e0
--remote <87405411+ly18028750128@users.noreply.github.com>
2024-01-14 14:38:38 +0800

    1：修改路由跟踪时的空指针错误

--- core-common-parent/feign-common/src/main/java/org/cloud/feign/config/FeignTracerConfiguration.java
+++ core-common-parent/feign-common/src/main/java/org/cloud/feign/config/FeignTracerConfiguration.java
@@ -2,15 +2,24 @@ package org.cloud.feign.config;
 
 import brave.Tracer;
 import feign.RequestInterceptor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Slf4j
 public class FeignTracerConfiguration {
     public final static String FEIGN_REQUEST_TRACER_ID = "feign_request_tracer_id";
 
     @Bean
     public RequestInterceptor feignTracerInterceptor(Tracer tracer) {
-        return requestTemplate -> requestTemplate.header(FEIGN_REQUEST_TRACER_ID, tracer.currentSpan().context().traceIdString());
+        return requestTemplate -> {
+            try{
+                requestTemplate.header(FEIGN_REQUEST_TRACER_ID, tracer.currentSpan().context().traceIdString());
+            }catch (Exception e){
+                log.warn("设置feign跟踪路由信息失败",e);
+            }
+
+        };
     }
 }
